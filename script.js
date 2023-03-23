9617b8edb94b7f92859669ee3490940505c3658d
Guilherme Andrade <guilhermesantosandrade01@gmail.com>
2023-03-23 15:58:23 -0300

    changes 1 (ex1 completed, ex2 html)

---
+++ script.js
@@ -0,0 +1,39 @@
+function ex1() {
+
+  let n1= document.getElementById("n1").value
+  let n2= document.getElementById("n2").value
+
+  let result= n1-n2
+  
+  alert("O resultado foi de: " +result)
+}
+
+function ex2() {
+
+
+}
+
+function ex3() {
+
+
+}
+
+function ex4() {
+
+
+}
+
+function ex5() {
+
+
+}
+
+function ex6() {
+
+
+}
+
+function ex7() {
+
+
+}
